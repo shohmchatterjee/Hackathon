abc0683adfb419701a38d54e93f46d80e5bf956b
codoin <kevinm0602@icloud.com>
2024-04-14 05:32:10 -0400

    push3

--- Assets/DoorScript.cs
+++ Assets/DoorScript.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//EXIT DOOR
 public class DoorScript : MonoBehaviour
 {
+    public VaultItemScript vault;
+    public GameObject door;
     // Start is called before the first frame update
     void Start()
     {
-        
+        vault = GameObject.FindGameObjectWithTag("vault").GetComponent<VaultItemScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if(vault.hasVaultItem){
+            Destroy(door);
+        }
     }
 }
